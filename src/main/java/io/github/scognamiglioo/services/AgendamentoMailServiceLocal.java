77b7556eef23e46a28c639d343cc90af65000c4b
julia veloso <85513215+scognamiglioo@users.noreply.github.com>
2025-12-10 18:54:45 -0300

    Add methods for cancelation and rescheduling in AgendamentoMailServiceLocal

--- src/main/java/io/github/scognamiglioo/services/AgendamentoMailServiceLocal.java
+++ src/main/java/io/github/scognamiglioo/services/AgendamentoMailServiceLocal.java
@@ -6,5 +6,10 @@ import jakarta.mail.MessagingException;
 
 @Local
 public interface AgendamentoMailServiceLocal {
+    
     void sendConfirmacaoAgendamento(Agendamento agendamento) throws MessagingException;
+    
+    void sendCancelamentoAgendamento(Agendamento agendamento) throws MessagingException;
+    
+    void sendReagendamento(Agendamento agendamentoOriginal, Agendamento novoAgendamento) throws MessagingException;
 }
